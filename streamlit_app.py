f7af65fa86f4b7b6ec95b590f886f75f50e19ac8
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2025-04-30 17:03:39 -0400

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-home_page = st.Page("Home.py", title = "Home")
-method_page = st.Page("Underlying_Workflow.py", title = "Methodology")
-
-pg = st.navigation([home_page, method_page])
-pg.run()
